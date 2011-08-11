72372e36ae33567c9ced97bd785be8507ed38542
Tjeerd van Dijk <tjeerd@thehyve.nl>
2011-08-11 15:43:52 +0200

    TagLib and css to facilitate tabs and fancy buttons in the importers

--- grails-app/conf/SamResources.groovy
+++ grails-app/conf/SamResources.groovy
@@ -5,6 +5,7 @@ modules = {
 		resource url: 'js/selectAddMore.js'
 		resource url: 'js/removeWebFlowExecutionKey.js'
 		resource url: 'css/sam.css'
+        resource url: 'css/importer.css'
 		resource url: 'css/tooltip.css'
         resource url:'/images/subjectlayout.png', attrs:[alt:''], disposition:'inline'
         resource url:'/images/samplelayout.png', attrs:[alt:''], disposition:'inline'
