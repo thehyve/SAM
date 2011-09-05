570d808d8942f111b73dbf2e5df7d43a3226ad86
RobertHorlings <robert@thehyve.nl>
2011-09-05 14:11:29 +0200

    Updated modulebase to improve performance of synchronization

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -3,9 +3,12 @@ dataSource {
     driverClassName = "org.hsqldb.jdbcDriver"
     username = "sa"
     password = ""
+	
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
     cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+	
+	jdbc.batch_size = 20
 }
