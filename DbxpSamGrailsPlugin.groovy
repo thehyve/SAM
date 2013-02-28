5c3c7a7e68aec915ebff97021d538280c8eca0c2
Ferry <FerryJagers@gmail.com>
2013-02-28 23:12:54 +0100

    version up

--- DbxpSamGrailsPlugin.groovy
+++ DbxpSamGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class DbxpSamGrailsPlugin {
     // the plugin version
-    def version = "0.9.4.4"
+    def version = "0.9.4.5"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
     // the other plugins this plugin depends on
